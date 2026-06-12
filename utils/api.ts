4f6d5e07c924d9b04adeb517ef2b965b76e5a332
janaka99 <janakachamith88@gmail.com>
2026-06-12 22:47:21 +0530

    Updated env file

--- utils/api.ts
+++ utils/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:8000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 export async function fetchAPI(endpoint: string, options: RequestInit = {}) {
   const token =
